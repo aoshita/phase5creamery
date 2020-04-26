7ec37e287133d0db8f947240e6bc4fb1f3242159
Alec Oshita <oshita.alec@gmail.com>
2020-04-25 20:57:16 -0400

    controller and views dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # just show a flash message instead of full CanCan exception
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "You are not authorized to take this action.  Go away or I shall taunt you a second time."
+    redirect_to home_path
+  end
+
   # handle 404 errors with an exception as well
   rescue_from ActiveRecord::RecordNotFound do |exception|
-
-    # consider creating your own 404 page within home and redirecting there...
-    
     flash[:error] = "Seek and you shall find... but not this time"
     redirect_to home_path
   end
-  
+
+  # private
+  # # Handling authentication
+  # def current_employee
+  #   @current_employee ||= Employee.find(session[:employee_id]) if session[:employee_id]
+  # end
+  # helper_method :current_employee
+  #
+  # def logged_in?
+  #   current_employee
+  # end
+  # helper_method :logged_in?
+  #
+  # def check_login
+  #   redirect_to login_path, alert: "You need to log in to view this page." if current_employee.nil?
+  # end
+
 end
