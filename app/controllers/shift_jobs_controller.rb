0549e382c55df47f068735d7706178fede1fe5d2
Alec Oshita <oshita.alec@gmail.com>
2020-04-27 19:56:13 -0400

    shifts and shift jobs

--- app/controllers/shift_jobs_controller.rb
+++ app/controllers/shift_jobs_controller.rb
@@ -4,6 +4,9 @@ class ShiftJobsController < ApplicationController
 
   def new
     @shift_job = ShiftJob.new
+    @shift     = Shift.find(params[:shift_id])
+    @shift_job.shift = @shift
+    #@shift_job.shift_id = params[:shift_id] unless params[:shift_id].nil?
   end
 
   def create
@@ -11,28 +14,36 @@ class ShiftJobsController < ApplicationController
     if @shift_job.save
       redirect_to shift_path(@shift_job.shift), notice: "Successfully added #{@shift_job.job.name} to #{@shift_job.shift.employee.name}'s shift'."
     else
-      render action: 'new'
+      #@shift = Shift.find(params[:shift_job][:shift_id])
+      render action: 'new'#, locals: {shift: @shift}
     end
   end
 
   def destroy
+    @shift     = Shift.find(params[:shift_id])
     if @shift_job.destroy
-      redirect_to shift_path(@shift_job.shift), notice: "Removed #{@shift_job.job.name} from #{@shift_job.shift.employee.name}'s shift."
+      redirect_to shift_path(@shift), notice: "Removed from #{@shift.employee.name}'s shift."
+    else
+      
+      render action: 'show'
+    end
+
+    #redirect_to shift_path(@shift)
     # else
       #redirect_to shift_path(@shift_job.shift
-    end
+
   end
 
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_shift_job
-    @shift_job = ShiftJob.find(params[:id])
-  end
+    def set_shift_job
+      @shift_job = ShiftJob.find(params[:id])
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def shift_job_params
-    params.require(:shift_job).permit(:shift_id, :job_id)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def shift_job_params
+      params.require(:shift_job).permit(:shift_id, :job_id)
+    end
 
 end
